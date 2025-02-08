e0c6f325c66a87632ba03bd3a1ab386c3e8973b7
aribeiroc <andersonspl@hotmail.com>
2025-02-08 08:32:13 -0300

    refatoring all code, cleaning garbage, insert description for the all functions.

--- file.py
+++ file.py
@@ -7,6 +7,8 @@ def fileNameToday():
     # TRATANDO O NOME DO ARQUIVO
     today = str(today.strftime("%d-%m-%Y"))
     nameFile = f'backup-{today}'
+
+    # RETORNANDO O NOME DO ARQUIVO COM A DATA E HORARIO ATUAL
     return nameFile
 
 def copyFile(fileBackup):
@@ -22,14 +24,26 @@ def copyFile(fileBackup):
 
 
 def replaceNameFileBackup():
+    # PEGANDO O NOME DO ARQUIVO DE BACKUP
     nameFile = fileNameToday()
+
+    # SETANDO O CAMINHO DA PASTA ONDE ESTA O ARQUIVO DE BACKUP DO SISTEMA
     path = "C:/wamp/www/_bkp"
+
+    # ARMAZENANDO OS NOMES DE TODOS OS ARQUIVOS DA PASTA EM UMA LISTA
     files = os.listdir(path)
+
+    # VERIFICANDO SE O NOME DO ARQUIVO DE BACKUP ESTA NA LISTA DE ARQUIVOS DA PASTA DE BACKUP DO SISTEMA
     for file in files:
+
+        # ARMAZENANDO O NOME DO ARQUIVO DE BACKUP DO DIA NA VARIÁVEL
         if file.find(nameFile) == 0:
             nameFileBackup = file
 
+    # COPIANDO O ARQUIVO PARA A PASTA RAIZ
     copyFile(nameFileBackup)
+
+    # RETORNA O NOME DO ARQUIVO DO BACKUP
     return nameFileBackup
 
 
--- main.py
+++ main.py
@@ -3,25 +3,46 @@ from doBackup import makeBackup
 from file import replaceNameFileBackup
 from sendEmail import sendEmail
 from time import sleep
+from datetime import datetime
 import schedule
 
-def initializeBackup():
-    makeBackup()
+# FUNÇÃO QUE VERIFICA A HORA DO DIA E DETERMINA SE SERÁ FEITO O BACKUP OU NÃO
+def makeBackOrNotMake():
+    # RECEBENDO A HORA ATUAL
+    date = datetime.now()
+
+    # CONVERTENDO EM STRING
+    date= str(date)
+
+    # DIVIDINDO A DATA DA HORA
+    date = date.split(" ")
+
+    # PEGANDO A HORA COMPLETA E RETIRANDO APENAS O VALOR DA HORA
+    date = date[1].split(':')
+
+    # CONVERTENDO O VALOR DA HORA EM INTEIRO
+    hora = int(date[0])
+
+    # VERIFICANDO SE A HORA ATUAL É MAIOR QUE 9
+    if hora > 16:
+        makeBackup()
 
 
+# FUNÇÃO QUE COPIA O ARQUIVO DE BACKUP PARA A RAIZ DA APLICAÇÃO
 def copyFileBackup():
+    makeBackOrNotMake()
     return replaceNameFileBackup()
 
+# FUNÇÃO QUE DISPARA O ENVIO DO EMAIL
 def sendEmailBackup():
-    file = copyFileBackup() 
+    file = copyFileBackup()
     sendEmail(file)
 
-schedule.every().day.at("17:52").do(initializeBackup)
 
-schedule.every().day.at('17:54').do(sendEmailBackup)
+schedule.every().day.at("09:00").do(sendEmailBackup)
+schedule.every().day.at("17:50").do(sendEmailBackup)
 
 while True:
     schedule.run_pending()
-    sleep(1)
-
-    
+    sleep(60)
+  
